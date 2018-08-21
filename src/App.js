39c64382bf6556df3e142f4cdbb6ac9597c03c76
Sular-Paul <sular_paul@yahoo.com>
2018-08-21 21:40:35 +0300

    Moved components to new folder

--- src/App.js
+++ src/App.js
@@ -1,29 +1,133 @@
 import React, { Component } from 'react';
-import Filter from './Filter';
-import GoogleMap from './GoogleMap';
-import Header from './Header';
+import Filter from './components/Filter';
+import GoogleMap from './components/GoogleMap';
+import Header from './components/Header';
 import escapeRegExp from 'escape-string-regexp';
+import * as LocationsAPI from './components/LocationsAPI';
 import './App.css';
-import * as LocationsAPI from './LocationsAPI';
 
 class App extends Component {
 	state = {
 		places: [],
 		query: '',
+		showingInfoWindow: false,
+		activeMarker: {},
+		selectedPlace: {},
 	};
 
+	// Handle marker click menu click	
+	onMarkerClick = (props, marker, e) => {
+
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfoWindow: true
+		});
+	}
+
+	// Handle hamburger menu click/enter key
+	onHamburgerClick = (e) => {
+		if (e.key === 'Enter' || !e.key) {
+			const menu = document.querySelector('.hamburger');
+
+			if (menu.getAttribute('aria-expanded') === 'true') {
+				menu.setAttribute('aria-expanded', 'false');
+				menu.setAttribute('aria-hidden', 'false');
+			} else {
+				menu.setAttribute('aria-expanded', 'true');
+				menu.setAttribute('aria-hidden', 'true');
+			}
+
+			document.querySelector('.App-header').classList.toggle('full-width');
+			document.querySelector('.map-container').classList.toggle('full-width');
+			document.querySelector('#filter').classList.toggle('hidden');
+		}
+	}
+
+	// Handle list item click/enter key
+	onListClick = (props, e) => {
+		if (e.key === 'Enter' || !e.key) {
+			let clickedMarker;
+			if (document.querySelectorAll('.gmnoprint map area').length !== 0) {
+				clickedMarker = [...document.querySelectorAll('.gmnoprint map area')];
+			} else {
+				clickedMarker = [...document.querySelectorAll('.gmnoprint')];
+			}
+			clickedMarker = clickedMarker.find(marker => marker.getAttribute('title') === props.venue);
+			clickedMarker.click();
+		}
+	}
+
+	// Handle map click
+	onMapClicked = () => {
+		if (this.state.showingInfoWindow) {
+			this.setState({
+				showingInfoWindow: false,
+				activeMarker: null
+			})
+		}
+	}
+
+	// Update searching query
 	updateQuery = (query) => {
 		this.setState({ query: query })
 	}
 
+	// Handle errors from map
+	handleMapErrors = () => {
+
+		window.gm_authFailure = function () {
+			document.querySelector('.map-container').innerHTML = `
+				<div class = "error-container">
+						<h3>Ooops... Something went wrong while trying to load the map!</h2>
+				</div>
+				`;
+		}
+
+		setTimeout(function () {
+			if (document.querySelector('.gm-err-container')) {
+				const errorContainer = document.querySelector('.gm-err-container');
+				const message = document.querySelector('.gm-err-message').innerText;
+				errorContainer.innerHTML = `
+					<div class = "error-container">
+						<h3>Something went wrong while trying to load the map!</h2>
+						<div class = "error-message">${message}</div>
+					</div>
+				`;
+			}
+
+			if (!document.querySelector('#map')) {
+				document.querySelector('.map-container').innerHTML = `
+				<div class = "error-container">
+						<h3>Something went wrong while trying to load the map!</h2>
+				</div>
+				`;
+			}
+		}, 1500);
+	}
+
+	// Handle errors from FourSquareAPI
+	handleFourSquareErrors = (error) => {
+		const errorContainer = document.querySelector('.list');
+		errorContainer.innerHTML = `
+					<div class = "error-container">
+						<h3>Something went wrong while trying to get places!</h2>
+						<div class = "error-message">${error}</div>
+					</div>
+				`;
+	}
+
+	// Get data from FourSquareAPI and call error handlers
 	componentDidMount() {
 		LocationsAPI.get().then(places => {
 			this.setState({ places })
 		})
+			.catch(error => this.handleFourSquareErrors(error))
+			.then(this.handleMapErrors())
 	}
 
 	render() {
-		
+
 		let showPlaces;
 		if (!this.state.query) {
 			showPlaces = this.state.places;
@@ -35,18 +139,24 @@ class App extends Component {
 
 		return (
 			<div className="App">
-				<div id="selector">
-				<h2>Cluj-Napoca popular places</h2>
-					Filter: <input
-						type="text"
-						placeholder="Search for locations"
-						value={this.state.query}
-						onChange={(event) => this.updateQuery(event.target.value)}
+				<Filter className="list-item"
+					query={this.state.query}
+					updateQuery={this.updateQuery}
+					places={showPlaces}
+					onListClick={this.onListClick}
+				/>
+				<Header
+					onHamburgerClick={this.onHamburgerClick}
+				/>
+				<div className="map-container">
+					<GoogleMap
+						places={showPlaces}
+						onMarkerClick={this.onMarkerClick}
+						activeMarker={this.state.activeMarker}
+						showingInfoWindow={this.state.showingInfoWindow}
+						selectedPlace={this.state.selectedPlace}
 					/>
-					<Filter places={showPlaces} />
 				</div>
-				<Header />
-				<GoogleMap places={showPlaces} />
 			</div>
 		);
 	}
